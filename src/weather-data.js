964a7d79bb361dacceabd1a01a2100884a4fb81c
Esa <esa-pitkanen@hotmail.com>
2025-07-24 13:12:27 +0300

    Handle errors from the API response
    Use a try catch block to handle errors, such as a location that does not exist.

--- src/weather-data.js
+++ src/weather-data.js
@@ -1,27 +1,41 @@
 class Weather {
   constructor(location, condition, temperature) {
-    (this.location = location),
-      (this.condition = condition),
-      (this.temperature = temperature);
+    this.location = location,
+    this.condition = condition,
+    this.temperature = Math.round((temperature - 32) * 5 / 9); // Convert to celsius
   }
 }
 
 async function getWeather(location) {
   const apiKey = "59MW3J2YZPNZMZWKMG5R4D4NM";
   let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}`;
+  
+  try {
+    const response = await fetch(url, { mode: "cors" });
 
-  const response = await fetch(url, { mode: "cors" });
-  const data = await response.json();
-  const locationData = data.currentConditions;
+    if(!response.ok) {
+      throw new Error(`HTTP Error: ${response.status}`);
+    }
 
-  const weather = new Weather(
-    location,
-    locationData.conditions,
-    locationData.temp,
-  );
+    const data = await response.json();
 
-  console.log(weather);
-  return weather;
+    const locationData = data.currentConditions;
+
+    // console.log(data);
+
+    const weather = new Weather(
+      location,
+      locationData.conditions,
+      locationData.temp,
+    );
+
+    console.log(weather);
+    return weather;
+
+  } catch (err) {
+    console.error("Error fetching weather:", err.message);
+    return null;
+  }
 }
 
 export { getWeather };
