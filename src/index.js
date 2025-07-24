e4372215e4e95b50bbb2b78a45b318565c0c2ef0
Esa <esa-pitkanen@hotmail.com>
2025-07-24 16:27:44 +0300

    Create a main function that handles data and UI
    An asynchronous function was created that processes the data, and based on the result, it is fed into the function that processes the UI.
    
    The form submission event was also added to the module.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import "./styles.css";
 import { getWeather } from "./weather-data";
+import { logger } from "./weather-ui";
 
-getWeather("helsinki");
+const input = document.querySelector("#location");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+    const data = input.value;
+
+    init(data);
+    e.preventDefault();
+})
+
+async function init(location) {
+  const data = await getWeather(location);
+
+  if (data) {
+    logger(data);
+  } else {
+    console.log("Weather data could not be loaded.");
+  }
+}
+
+init("helsinki")
