cec304c27b57cc0a5dfa59e9bdcc249c2ae90a55
Esa <esa-pitkanen@hotmail.com>
2025-07-26 16:34:43 +0300

    Add a module for retrieving images
    Added a function that retrieves an image from Giphy.

---
+++ src/weather-gif.js
@@ -0,0 +1,11 @@
+async function getWeatherImg(weather) {
+  const apiKey = "JsTvY8DZjCDCPimPNeQMhxRKuiSYw5x0";
+  const url = `https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${weather}`;
+
+  const response = await fetch(url, { mode: "cors" });
+  const data = await response.json();
+  const source = data.data.images.original.url;
+  return source;
+}
+
+export { getWeatherImg }
