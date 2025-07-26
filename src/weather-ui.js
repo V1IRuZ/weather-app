7f3735bd9ced1d4d43ac998c742799579a70d2d3
Esa <esa-pitkanen@hotmail.com>
2025-07-26 16:42:57 +0300

    Add a function to display an image

--- src/weather-ui.js
+++ src/weather-ui.js
@@ -1,7 +1,32 @@
-function logger(obj) {
+import { createPara, resetDOM } from "./utils";
+
+const container = document.querySelector(".content");
+const tempWrapper = document.querySelector(".temp-wrapper");
+const infoWrapper = document.querySelector(".info");
+
+function displayWeatherInfo(obj) {
+  resetDOM(tempWrapper);
+  resetDOM(infoWrapper);
+  const location = createPara(obj.location, "location");
+  infoWrapper.appendChild(location);
+
+  const condition = createPara(obj.condition, "condition");
+  infoWrapper.appendChild(condition);
+
+  const temperature = document.createElement("span")
+  temperature.innerHTML = `${obj.temperature}&deg`;
+  temperature.classList.add("temperature")
+  tempWrapper.appendChild(temperature);
+
   console.log(`Location: ${obj.location}`);
   console.log(`Condition: ${obj.condition}`);
   console.log(`Temperature: ${obj.temperature}`);
 }
 
-export { logger };
+const img = document.querySelector("img");
+
+function displayImg(imgSrc) {
+  img.src = imgSrc;
+}
+
+export { displayWeatherInfo, displayImg };
