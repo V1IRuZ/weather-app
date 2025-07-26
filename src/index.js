f01d06459414f3f38c3f1862b271f34cff1e87a8
Esa <esa-pitkanen@hotmail.com>
2025-07-26 16:49:34 +0300

    Add image functions to the main function
    The image is added from Giphy, based on data from the weather API.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./styles.css";
 import { getWeather } from "./weather-data";
-import { logger } from "./weather-ui";
+import { displayWeatherInfo, displayImg } from "./weather-ui";
+import { getWeatherImg } from "./weather-gif";
+import { getTempString } from "./utils";
 
 const input = document.querySelector("#location");
 const form = document.querySelector("form");
@@ -13,10 +15,13 @@ form.addEventListener("submit", (e) => {
 })
 
 async function init(location) {
-  const data = await getWeather(location);
-
-  if (data) {
-    logger(data);
+  const weatherData = await getWeather(location);
+  
+  if (weatherData) {
+    const imgSrc = getTempString(weatherData);
+    const imgData = await getWeatherImg(imgSrc);
+    displayWeatherInfo(weatherData);
+    displayImg(imgData);
   } else {
     console.log("Weather data could not be loaded.");
   }
