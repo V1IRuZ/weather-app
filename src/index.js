2e26fc6550cd06a538325e336a3bb5ad961234ef
Esa <esa-pitkanen@hotmail.com>
2025-07-28 19:52:54 +0300

    Call the weather icon function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 import { getWeather } from "./weather-data";
-import { displayWeatherInfo, displayImg } from "./weather-ui";
+import { displayWeatherInfo, displayImg, displayWeatherIcon } from "./weather-ui";
 import { getWeatherImg } from "./weather-gif";
 import { getTempString } from "./utils";
 
@@ -8,23 +8,24 @@ const input = document.querySelector("#location");
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
-    const data = input.value;
+  const data = input.value;
 
-    init(data);
-    e.preventDefault();
-})
+  init(data);
+  e.preventDefault();
+});
 
 async function init(location) {
   const weatherData = await getWeather(location);
-  
+
   if (weatherData) {
     const imgSrc = getTempString(weatherData);
     const imgData = await getWeatherImg(imgSrc);
     displayWeatherInfo(weatherData);
+    await displayWeatherIcon(weatherData);
     displayImg(imgData);
   } else {
     console.log("Weather data could not be loaded.");
   }
 }
 
-init("helsinki")
+init("helsinki");
