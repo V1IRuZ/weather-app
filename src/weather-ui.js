4f98f8bd3c0096bde9986db2c00239f3298a33d4
Esa <esa-pitkanen@hotmail.com>
2025-07-30 18:28:02 +0300

    Create a function that displays the weather for the week
    For the weather of the week, a card had to be created for each day to contain the content. To do this, the weather icon insertion function had to be modified slightly so that it could also be used for days of the week.
    
    The date-fns package was downloaded for date formatting.

--- src/weather-ui.js
+++ src/weather-ui.js
@@ -1,17 +1,43 @@
 import { createPara, resetDOM, toggleTemperatureUnit } from "./utils";
+import { format } from "date-fns";
 
 const tempWrapper = document.querySelector(".temp-wrapper");
 const infoWrapper = document.querySelector(".info");
 const toggleBtn = document.querySelector(".toggle");
+const weekDaysContainer = document.querySelector(".week");
+
+async function makeWeatherDayCard(day) {
+  const card = document.createElement("div");
+  card.classList.add("day-card");
+
+  const date = document.createElement("p");
+  const parsedDate = new Date(day.date);
+  const formattedDate = format(parsedDate, "d.M")  
+  date.textContent = `${formattedDate}`;
+  card.appendChild(date);
+
+  await displayWeatherIcon(day.icon, card);
+
+  const temp = document.createElement("p");
+  temp.innerHTML = `${day.minCelsius} / ${day.maxCelsius}&deg;C`;
+  card.appendChild(temp);
+
+  return card
+}
+
+async function displayWeeklyWeather(daysData) {
+  resetDOM(weekDaysContainer);
+  for(const day of daysData) {
+    const card = await makeWeatherDayCard(day);
+    weekDaysContainer.appendChild(card);
+  }
+}
 
 function displayWeatherInfo(obj) {
   resetDOM(tempWrapper);
   resetDOM(infoWrapper);
   const location = createPara(obj.location, "location");
   infoWrapper.appendChild(location);
-
-  // const condition = createPara(obj.condition, "condition");
-  // infoWrapper.appendChild(condition);
 
   const temperature = document.createElement("span");
   temperature.innerHTML = `${obj.celsius}&deg;C`;
@@ -23,15 +49,15 @@ function displayWeatherInfo(obj) {
   });
 }
 
-async function displayWeatherIcon(obj) {
+async function displayWeatherIcon(iconName, container) {
   try {
-    const icon = await import(`./icons/${obj.condition}.svg`)
+    const icon = await import(`./icons/${iconName}.svg`)
     const iconPath = icon.default;
 
     const weatherIcon = document.createElement("img");
     weatherIcon.src = iconPath;
-    weatherIcon.alt = `${obj.condition}`;
-    infoWrapper.appendChild(weatherIcon);
+    weatherIcon.alt = `${iconName}`;
+    container.appendChild(weatherIcon);
 
   } catch (err) {
     console.error("Icon failed to load:", err);
@@ -44,4 +70,4 @@ function displayImg(imgSrc) {
   img.src = imgSrc;
 }
 
-export { displayWeatherInfo, displayImg, displayWeatherIcon };
+export { displayWeatherInfo, displayWeeklyWeather, displayImg, displayWeatherIcon, infoWrapper };
